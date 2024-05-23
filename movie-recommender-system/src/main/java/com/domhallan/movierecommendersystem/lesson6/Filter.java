dc0490b11dcbfed37211a4328291b106eb7d0710
Dom Hallan <dom@domhallan.com>
2024-05-23 18:30:35 -0400

    Updated package reference in Filter interface
    The package reference in the Filter interface has been updated. The previous reference was to lesson5, but it has now been changed to lesson6. This change ensures that the Filter interface is correctly located within the project structure.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson6/Filter.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson6/Filter.java
@@ -1,4 +1,4 @@
-package com.domhallan.movierecommendersystem.lesson5;
+package com.domhallan.movierecommendersystem.lesson6;
 
 public interface Filter {
   public String[] getRecommendations(String movie);
