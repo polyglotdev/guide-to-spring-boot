c2e82f01236938881ea96d1b47e97137a60bdba8
Dom Hallan <dom@domhallan.com>
2024-05-23 18:34:36 -0400

    Enhanced content-based filter with qualifiers
    Added Spring's @Qualifier annotation to the ContentBasedFilter class. This allows for more precise autowiring of beans in the application context, improving code clarity and reducing potential errors.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson6/ContentBasedFilter.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson6/ContentBasedFilter.java
@@ -1,8 +1,10 @@
 package com.domhallan.movierecommendersystem.lesson6;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("CBF")
 public class ContentBasedFilter implements Filter {
   public String[] getRecommendations(String movie) {
     //logic of content based filter
