9dd9a95b4df1ac8b74a863ab95bf5ab45fef5894
Dom Hallan <dom@domhallan.com>
2024-05-23 17:59:18 -0400

    Enhanced CollaborativeFilter class
    Added Javadoc comments to the CollaborativeFilter class and its getRecommendations method. This provides a clearer understanding of the class's purpose and functionality, specifically how it uses collaborative filtering to generate movie recommendations.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson4/CollaborativeFilter.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson4/CollaborativeFilter.java
@@ -3,9 +3,19 @@ package com.domhallan.movierecommendersystem.lesson4;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+/**
+ * Collaborative filtering implementation of the Filter interface
+ */
 @Component
 @Primary
 public class CollaborativeFilter implements Filter {
+
+  /**
+   * Provides movie recommendations based on collaborative filtering.
+   *
+   * @param movie - the movie for which recommendations are needed.
+   * @return array of recommended movie titles.
+   */
   public String[] getRecommendations(String movie) {
     //logic of content based filter
     return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
