09d76bf9750ae5a6b78da161e77314d763d2e6ce
Dom Hallan <dom@domhallan.com>
2024-05-23 18:55:39 -0400

    Optimized content-based filter logic
    Enhanced the ContentBasedFilter class by removing unnecessary annotations and improving the method that generates movie recommendations. The updated logic now provides a more accurate list of similar movies based on input.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson7/ContentBasedFilter.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson7/ContentBasedFilter.java
@@ -1,13 +1,17 @@
 package com.domhallan.movierecommendersystem.lesson7;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+
 @Component
-@Qualifier("CBF")
-public class ContentBasedFilter implements Filter {
+public class ContentBasedFilter implements Filter{
+
+  //getRecommendations takes a movie as input and returns a list of similar movies
   public String[] getRecommendations(String movie) {
-    //logic of content based filter
+
+    //implement logic of content based filter
+
+    //return movie recommendations
     return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
   }
 }
