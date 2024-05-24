6233e1a2ae03fa07126dc7299140c68c14a1f3bf
Dom Hallan <dom@domhallan.com>
2024-05-24 09:41:51 -0400

    Updated bean scope and constructor in Movie class
    The changes include an update to the bean scope annotation in the Movie class, switching from a simple prototype declaration to a more detailed one that includes proxy mode. Additionally, a call to super() was added in the Movie constructor.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson9/Movie.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson9/Movie.java
@@ -1,10 +1,13 @@
 package com.domhallan.movierecommendersystem.lesson9;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(value= ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Movie {
   private static int instances = 0;
 
@@ -14,6 +17,7 @@ public class Movie {
   private String producer;
 
   public Movie() {
+    super();
     instances++;
     System.out.println("Movie constructor called");
   }
