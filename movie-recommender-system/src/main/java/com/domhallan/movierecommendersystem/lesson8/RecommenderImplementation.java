327b34c6ae25958fbf4c93c351e05cad6fe37ef0
Dom Hallan <dom@domhallan.com>
2024-05-24 07:40:20 -0400

    Updated package reference in RecommenderImplementation
    The package reference in the RecommenderImplementation class has been updated. It was previously pointing to lesson7, but it now correctly points to lesson8. This change ensures that the class is properly located within the project structure.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson8/RecommenderImplementation.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson8/RecommenderImplementation.java
@@ -1,4 +1,4 @@
-package com.domhallan.movierecommendersystem.lesson7;
+package com.domhallan.movierecommendersystem.lesson8;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
