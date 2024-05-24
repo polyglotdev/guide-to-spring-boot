bb640230a3ced585350efc328ae6780a4d3bc9c3
Dom Hallan <dom@domhallan.com>
2024-05-24 09:34:03 -0400

    Enhanced Movie class with new properties and methods
    The Movie class has been significantly expanded. New private fields for id, name, genre, and producer have been added. A constructor that increments an instance counter each time it's called was also introduced. Additionally, a static method to get the number of instances and a method to calculate movie similarity based on genre and producer were implemented.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson9/Movie.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson9/Movie.java
@@ -1,2 +1,32 @@
-package com.domhallan.movierecommendersystem.lesson9;public class Movie {
+package com.domhallan.movierecommendersystem.lesson9;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Movie {
+  private static int instances = 0;
+
+  private int id;
+  private String name;
+  private String genre;
+  private String producer;
+
+  public Movie() {
+    instances++;
+    System.out.println("Movie constructor called");
+  }
+
+  public static int getInstances(){
+    return Movie.instances;
+  }
+
+  public double movieSimilarity(int movie1, int movie2) {
+
+    //if genres are same add 0.3 to similarity
+    //if producers are same add 0.5 to similarity
+
+    return 0.0;
+  }
 }
