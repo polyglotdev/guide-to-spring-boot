e7a26074ee17a36bff08b584fbcc33b3acdc5915
Dom Hallan <dom@domhallan.com>
2024-05-24 09:08:03 -0400

    Improved content-based filter initialization
    Added a constructor to the ContentBasedFilter class. This change ensures that the object is properly initialized when created. Also, a print statement has been added for debugging purposes to confirm when the constructor is called.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson8/ContentBasedFilter.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson8/ContentBasedFilter.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class ContentBasedFilter implements Filter {
 
+  public ContentBasedFilter() {
+    super();
+    System.out.println("content-based filter constructor called");
+  }
+
   //getRecommendations takes a movie as input and returns a list of similar movies
   public String[] getRecommendations(String movie) {
 
