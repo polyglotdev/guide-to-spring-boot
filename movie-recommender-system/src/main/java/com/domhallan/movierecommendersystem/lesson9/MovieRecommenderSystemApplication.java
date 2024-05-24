7d549f36f03c87bfe6b8e994461c66fea8fccc6b
Dom Hallan <dom@domhallan.com>
2024-05-24 09:15:34 -0400

    Updated package reference
    The package reference in the main application file has been updated to reflect the current lesson number. This change ensures that the application is correctly associated with its corresponding lesson module.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson9/MovieRecommenderSystemApplication.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson9/MovieRecommenderSystemApplication.java
@@ -1,4 +1,4 @@
-package com.domhallan.movierecommendersystem.lesson8;
+package com.domhallan.movierecommendersystem.lesson9;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
