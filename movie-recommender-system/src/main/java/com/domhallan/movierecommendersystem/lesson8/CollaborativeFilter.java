920784a117a7b007bf3b7b4e7d167ad7d5a1b010
Dom Hallan <dom@domhallan.com>
2024-05-24 07:39:25 -0400

    Added movie recommendation functionality
    Implemented a movie recommendation system using collaborative and content-based filtering. Created an interface for the filters and two implementations of it, one for each type of filtering. Also added two classes that use these filters to recommend movies, demonstrating both constructor and setter injection methods.

---
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson8/CollaborativeFilter.java
@@ -0,0 +1,21 @@
+package com.domhallan.movierecommendersystem.lesson7;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Collaborative filtering implementation of the Filter interface
+ */
+@Component
+public class CollaborativeFilter implements Filter {
+
+  /**
+   * Provides movie recommendations based on collaborative filtering.
+   *
+   * @param movie - the movie for which recommendations are needed.
+   * @return array of recommended movie titles.
+   */
+  public String[] getRecommendations(String movie) {
+    //logic of content based filter
+    return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
+  }
+}
---
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson8/Filter.java
@@ -0,0 +1,5 @@
+package com.domhallan.movierecommendersystem.lesson7;
+
+public interface Filter {
+  String[] getRecommendations(String movie);
+}
