c38c94e6ff50d324f73de1d2ce8476de3db628b2
Dom Hallan <dom@domhallan.com>
2024-05-24 08:50:21 -0400

    Optimized movie recommendation system
    Removed unnecessary print statements from the RecommenderImplementation class. This change enhances code readability and performance by eliminating redundant console outputs. The constructor no longer prints a message upon invocation, and the name of the filter in use is also not printed during movie recommendations.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson8/RecommenderImplementation.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson8/RecommenderImplementation.java
@@ -14,14 +14,13 @@ public class RecommenderImplementation {
   public RecommenderImplementation(@Qualifier("collaborativeFilter") Filter filter) {
     super();
     this.filter = filter;
-    System.out.println("Constructor invoked...");
   }
 
   //use the injected filter to find recommendations
   public String [] recommendMovies (String movie) {
 
     //print the name of interface implementation being used
-    System.out.println("\nName of the filter in use: " + filter + "\n");
+    //System.out.println("\nName of the filter in use: " + filter + "\n");
     return filter.getRecommendations("Finding Dory");
   }
 
