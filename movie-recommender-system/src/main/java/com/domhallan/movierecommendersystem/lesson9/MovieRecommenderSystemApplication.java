b65a9b238c68866a7c00f7db2903c469599801ad
Dom Hallan <dom@domhallan.com>
2024-05-25 18:54:57 -0400

    Refactored bean retrieval in main application
    The main application's code has been refactored to simplify the process of retrieving beans. The previous implementation retrieved singleton and prototype beans separately, printed them out, and also displayed the number of instances created for each bean. This has been replaced with a more streamlined approach that checks if specific beans exist within the application context.

--- movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson9/MovieRecommenderSystemApplication.java
+++ movie-recommender-system/src/main/java/com/domhallan/movierecommendersystem/lesson9/MovieRecommenderSystemApplication.java
@@ -11,24 +11,7 @@ public class MovieRecommenderSystemApplication {
 
 		//ApplicationContext manages the beans and dependencies
 		ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
-
-		//Retrieve singleton bean from application context
-		ContentBasedFilter filter = appContext.getBean(ContentBasedFilter.class);
-		System.out.println("\nContentBasedFilter bean with singleton scope");
-		System.out.println(filter);
-
-		//Retrieve prototype bean from the singleton bean thrice
-		Movie movie1 = filter.getMovie();
-		Movie movie2 = filter.getMovie();
-		Movie movie3 = filter.getMovie();
-
-		System.out.println("\nMovie bean with prototype scope");
-		System.out.println(movie1);
-		System.out.println(movie2);
-		System.out.println(movie3);
-
-		//Print number of instances of each bean
-		System.out.println("\nContentBasedFilter instances created: " + ContentBasedFilter.getInstances());
-		System.out.println("Movie instances created: "+ Movie.getInstances());
+		System.out.println("ContentBasedFilter bean found = " + appContext.containsBean("contentBasedFilter"));
+		System.out.println("CollaborativeFilter bean found = " + appContext.containsBean("collaborativeFilter"));
 	}
 }
