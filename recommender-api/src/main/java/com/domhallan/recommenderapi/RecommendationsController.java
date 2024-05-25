91e56d7938c28a3c25e6fe5398568883c0fa680e
Dom Hallan <dom@domhallan.com>
2024-05-25 19:42:46 -0400

    Added new dependencies and endpoint
    New dependencies have been added to the pom.xml file, including spring-boot-devtools, spring-boot-starter-actuator, and spring-data-rest-hal-browser. In addition, a new GetMapping annotation has been introduced in the RecommendationsController class for retrieving all movies. Lastly, management endpoints exposure has been updated in application.properties file.

--- recommender-api/src/main/java/com/domhallan/recommenderapi/RecommendationsController.java
+++ recommender-api/src/main/java/com/domhallan/recommenderapi/RecommendationsController.java
@@ -1,5 +1,6 @@
 package com.domhallan.recommenderapi;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -8,6 +9,7 @@ import java.util.List;
 @RestController
 public class RecommendationsController {
 
+  @GetMapping("movies")
   public List<Movie> getAllMovies() {
     return Arrays.asList(new Movie(1, "Ice Age", 7.5),
         new Movie(2, "Happy Feet", 6.4),
