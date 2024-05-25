7fe8ec7e766757caf181ee5e752d383c82a0c00e
Dom Hallan <dom@domhallan.com>
2024-05-25 19:11:42 -0400

    Added Maven wrapper and initial project setup
    - Introduced Maven wrapper to the project for consistent build environment
    - Created a new Spring Boot application with basic dependencies (web, test)
    - Added a Movie class with id, name, and rating attributes
    - Implemented RecommendationsController with a method to return hardcoded list of movies
    - Configured application properties with the application name

---
+++ recommender-api/src/test/java/com/domhallan/recommenderapi/RecommenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domhallan.recommenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
